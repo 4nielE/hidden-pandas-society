8903727ddc2d019b60020632bff2c3fe75f5e885
anielespi <aniel.espinosa@protonmail.com>
2022-08-22 18:04:53 +0200

    corrected id vision

--- src/pages/Vision.js
+++ src/pages/Vision.js
@@ -33,8 +33,8 @@ function Vision() {
             </div>
             <div id="content-vision-section">
                 <div id="content-vision-title">
-                    <h1 id="h1-vision">What that means.</h1>
-                    <h2 id="h2-vision">Tangible benefits.</h2>
+                    <h1 id="h1-vision-middle-title">What that means.</h1>
+                    <h2 id="h2-vision-middle-subtitle">Tangible benefits.</h2>
                 </div>
                 <div id="content-vision-4-benefits">
                     <div id="content-vision-4-benefits-row">
